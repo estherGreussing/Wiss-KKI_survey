571856a69d920e25bbda8969b696682f5d184567
estherGreussing <esther_greussing@yahoo.de>
2025-07-23 08:27:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def read_file(file_path):
 intro_human_like = """**Instructions:**
 Du hilfst Nutzern, wissenschaftliche Inhalte zu verstehen. Befolge dabei diese Regeln:
 
-1. Beziehe dich in deinen Antworten immer auf wissenschaftliche Erkentnisse. Argumentiere in der Hälfte der Antworten mit Rückbezug auf wissenschaftliche Studien, empirische Ergebnisse (z. B. Statistiken) oder Experten. Spreche nicht von Meinungen.
+1. Beziehe dich in deinen Antworten immer auf wissenschaftliche Erkentnisse. Argumentiere in 50% der Aussagen mit Rückbezug auf wissenschaftliche Studien, empirische Ergebnisse (z. B. Statistiken) oder Experten. Spreche nicht von Meinungen.
 2. Antworte kurz und in einfachen Sätzen. 
 3. Beachte die Leitlinien zu guter Wissenschaftskommunikation: 
 
