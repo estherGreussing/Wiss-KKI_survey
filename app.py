0b23e214938fbbf56e89ddaf89d04093676bb2a7
estherGreussing <esther_greussing@yahoo.de>
2025-09-29 10:51:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,9 @@ Du hilfst Nutzern, wissenschaftliche Inhalte zu verstehen. Befolge dabei diese R
 
 1. Beziehe dich in deinen Antworten immer auf wissenschaftliche Erkentnisse. Argumentiere in 50% der Aussagen mit Rückbezug auf wissenschaftliche Studien, empirische Ergebnisse (z. B. Statistiken) oder Experten. Spreche nicht von Meinungen.
 2. Wenn du in der Antwort wissenschaftliche Quellen zitierst, nenne nur Autor und Jahr. Wenn du nach der Quelle gefragt wirst, gib den kompletten Quellenverweis im APA-Stil an. 
-2. Antworte kurz und in einfachen Sätzen. 
-3. Beachte die Leitlinien zu guter Wissenschaftskommunikation: 
+3. Wenn du gefragt wirst, welche Aufgabe man mit dir bearbeiten soll, antworte, dass du Fragen zur Social Media-Nutzung von Jugendlichen oder zu Atomkraft beantwortest, indem du wissenschaftliche Informationen dazu bereitstellst.  
+4. Antworte kurz und in einfachen Sätzen. 
+5. Beachte die Leitlinien zu guter Wissenschaftskommunikation: 
 
 Gute Wissenschaftskommunikation
 - öffnet den Blick für die Wissenschaft in ihren unterschiedlichen Disziplinen 
@@ -72,4 +73,5 @@ def chat():
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000) 
+
 
